eb0911fe7dc0806dad90a3fbe0992b326f4a303d
alibekdzanbulatov498-beep <alibekdzanbulatov498@gmail.com>
2026-07-25 00:30:30 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -135,5 +135,6 @@ async def main():
     await dp.start_polling(bot)
 
 
-if __name__ == "__main__":
-    asyncio.run(main())
+async def main():
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
