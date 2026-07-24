117f5dbcebe1d8082814a2e20374899517e0ede6
alibekdzanbulatov498-beep <alibekdzanbulatov498@gmail.com>
2026-07-24 23:30:20 +0500

    Create database.py

---
+++ database.py
@@ -0,0 +1,44 @@
+import sqlite3
+
+def init_db():
+    conn = sqlite3.connect("users.db")
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS users(
+        id INTEGER PRIMARY KEY,
+        username TEXT,
+        first_name TEXT,
+        xp INTEGER DEFAULT 0,
+        level INTEGER DEFAULT 1,
+        streak INTEGER DEFAULT 0,
+        completed INTEGER DEFAULT 0
+    )
+    """)
+
+    conn.commit()
+    conn.close()
+
+
+def add_user(user_id, username, first_name):
+    conn = sqlite3.connect("users.db")
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "INSERT OR IGNORE INTO users(id, username, first_name) VALUES(?,?,?)",
+        (user_id, username, first_name),
+    )
+
+    conn.commit()
+    conn.close()
+
+
+def get_user(user_id):
+    conn = sqlite3.connect("users.db")
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM users WHERE id=?", (user_id,))
+    user = cursor.fetchone()
+
+    conn.close()
+    return user
