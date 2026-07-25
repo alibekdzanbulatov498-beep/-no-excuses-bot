b01226fb271e210f8a26c6f8324a87cc08653d53
alibekdzanbulatov498-beep <alibekdzanbulatov498@gmail.com>
2026-07-26 01:47:59 +0500

    Update database.py

--- database.py
+++ database.py
@@ -1,7 +1,14 @@
 import sqlite3
 
+DB_NAME = "users.db"
+
+
+def get_connection():
+    return sqlite3.connect(DB_NAME)
+
+
 def init_db():
-    conn = sqlite3.connect("users.db")
+    conn = get_connection()
     cursor = conn.cursor()
 
     cursor.execute("""
@@ -21,51 +28,75 @@ def init_db():
 
 
 def add_user(user_id, username, first_name):
-    conn = sqlite3.connect("users.db")
+    conn = get_connection()
     cursor = conn.cursor()
 
-    cursor.execute(
-        "INSERT OR IGNORE INTO users(id, username, first_name) VALUES(?,?,?)",
-        (user_id, username, first_name),
-    )
+    cursor.execute("""
+        INSERT OR IGNORE INTO users
+        (id, username, first_name)
+        VALUES (?, ?, ?)
+    """, (user_id, username, first_name))
 
     conn.commit()
     conn.close()
 
 
 def get_user(user_id):
-    conn = sqlite3.connect("users.db")
-    cursor = conn.cursor()
-
-    cursor.execute("SELECT * FROM users WHERE id=?", (user_id,))
-    user = cursor.fetchone()
-
-    conn.close()
-    return user
-
-def add_xp(user_id, amount):
-    conn = sqlite3.connect("users.db")
+    conn = get_connection()
     cursor = conn.cursor()
 
     cursor.execute(
-        """
-        UPDATE users
-        SET xp = xp + ?,
-            completed = completed + 1
-        WHERE id = ?
-        """,
-        (amount, user_id)
+        "SELECT * FROM users WHERE id=?",
+        (user_id,)
     )
+
+    user = cursor.fetchone()
+
+    conn.close()
+
+    return user
+
+
+def add_xp(user_id, xp):
+
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        UPDATE users
+        SET
+            xp = xp + ?,
+            completed = completed + 1
+        WHERE id=?
+    """, (xp, user_id))
+
+    cursor.execute(
+        "SELECT xp FROM users WHERE id=?",
+        (user_id,)
+    )
+
+    current_xp = cursor.fetchone()[0]
+
+    level = current_xp // 100 + 1
+
+    cursor.execute("""
+        UPDATE users
+        SET level=?
+        WHERE id=?
+    """, (level, user_id))
 
     conn.commit()
     conn.close()
+
+
 def count_users():
-    conn = sqlite3.connect("users.db")
+    conn = get_connection()
     cursor = conn.cursor()
 
     cursor.execute("SELECT COUNT(*) FROM users")
-    count = cursor.fetchone()[0]
+
+    total = cursor.fetchone()[0]
 
     conn.close()
 
-    return count
+    return total
