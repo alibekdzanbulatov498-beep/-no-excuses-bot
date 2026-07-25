8749d826720e2e9556e4b6a9c06ac1de9ff2f9dc
alibekdzanbulatov498-beep <alibekdzanbulatov498@gmail.com>
2026-07-25 12:16:52 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -165,6 +165,9 @@ async def web_server():
 async def main():
 
     print("🔥 BOT STARTED")
+    
+me = await bot.get_me()
+print(f"Запущен бот: @{me.username}")
 
     await bot.delete_webhook(drop_pending_updates=True)
 
